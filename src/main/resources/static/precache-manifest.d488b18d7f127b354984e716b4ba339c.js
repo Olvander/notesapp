8cbf969ef7f62aa239105fd3240c67ccfa3ebd93
Olli Pertovaara <olli.pertovaara@gmail.com>
2020-09-05 20:29:50 +0300

    Build frontend and move files under static dir

---
+++ src/main/resources/static/precache-manifest.d488b18d7f127b354984e716b4ba339c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a4de051c2281b985462002da78358f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72fc06be0762ee5097ca",
+    "url": "/static/css/main.020d2fd5.chunk.css"
+  },
+  {
+    "revision": "e73bf9db288d34857a4c",
+    "url": "/static/js/2.10b21e0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10b21e0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72fc06be0762ee5097ca",
+    "url": "/static/js/main.4c217407.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
