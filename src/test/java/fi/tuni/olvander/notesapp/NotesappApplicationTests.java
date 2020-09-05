0476b699e06febe04af7b523e0ca28b3c2559d07
Olli Pertovaara <olli.pertovaara@gmail.com>
2020-09-05 14:49:59 +0300

    Project initialization

---
+++ src/test/java/fi/tuni/olvander/notesapp/NotesappApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.tuni.olvander.notesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
