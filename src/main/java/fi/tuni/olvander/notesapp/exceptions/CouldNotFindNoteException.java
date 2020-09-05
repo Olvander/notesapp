4f1e8c35e023a13533c7e010f811428c3610c2b6
Olli Pertovaara <olli.pertovaara@gmail.com>
2020-09-05 17:27:20 +0300

    Add an exception class to backend

--- src/main/java/fi/tuni/olvander/notesapp/exceptions/CouldNotFindNoteException.java
+++ src/main/java/fi/tuni/olvander/notesapp/exceptions/CouldNotFindNoteException.java
@@ -1,17 +1,4 @@
 package fi.tuni.olvander.notesapp.exceptions;
 
-/**
- * An instance of this class is created when a note cannot be found with an id.
- */
-public class CouldNotFindNoteException extends IllegalArgumentException {
-
-    private Long noteId;
-
-    public CouldNotFindNoteException(Long id) {
-        noteId = id;
-    }
-
-    public Long getNoteId() {
-        return noteId;
-    }
+public class CouldNotFindNoteException {
 }
